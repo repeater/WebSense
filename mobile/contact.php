9baba6071cb3425dfd084e803d8f84b1ce6e02e9
Craig Godfrey <repeater@gmail.com>
2013-02-08 18:05:48 -0800

    update to POCs
    just adding different elements to view
    
    Signed-off-by: Craig Godfrey <repeater@gmail.com>

--- mobile/contact.php
+++ mobile/contact.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+L for it<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
@@ -8,7 +8,7 @@
 <!--	<link rel="stylesheet" type="text/css" href="http://code.jquery.com/mobile/1.0b3/jquery.mobile-1.0b3.min.css" />-->
 	
 	<link rel="stylesheet" type="text/css" href="http://jquerymobile.com/test/css/themes/default/jquery.mobile.css" />
-	<link rel="stylesheet" type="text/css" href="http://jquerymobile.com/test/docs/_assets/css/jqm-docs.css" />
+	
 <!--	<script src="http://code.jquery.com/jquery-1.6.2.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.0b3/jquery.mobile-1.0b3.min.js"></script>-->
 	
@@ -17,46 +17,55 @@
 	<script src="http://jquerymobile.com/test/docs/_assets/js/jqm-docs.js"></script>
 	<script src="http://jquerymobile.com/test/js/jquery.mobile.js"></script>
 
-	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDKpCpi_bRs2NU8q5yGJ37y45Ls_lEr1Bs&sensor=false"></script>
-    <script>
-      function initialize() {
-        var myLatlng = new google.maps.LatLng(32.890501, -117.210278);
-        var mapOptions = {
-          zoom: 13,
-          center: myLatlng,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
 
-        var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&libraries=places&key=AIzaSyDKpCpi_bRs2NU8q5yGJ37y45Ls_lEr1Bs"></script> 
+	<script src="http://jquery-ui-map.googlecode.com/svn/trunk/ui/jquery.ui.map.js"></script>
+	<script src="http://jquery-ui-map.googlecode.com/svn/trunk/ui/jquery.ui.map.services.js"></script>
+	<script src="http://jquery-ui-map.googlecode.com/svn/trunk/ui/jquery.ui.map.extensions.js"></script>
+    <script type="text/javascript">
+				
+			var mobileDemo = { 'center': '32.890501, -117.210278', 'zoom': 14 };
+			
+			////////////////////////////////////////////////////////////
+			
+			$(function() {
+				$('#map_canvas_1').gmap({'center': mobileDemo.center, 'zoom': mobileDemo.zoom, 'disableDefaultUI':true, 'callback': function() {
+					var self = this;
+					
+					// add marker
+					self.addMarker({'position': this.get('map').getCenter() }).click(function() {
+						self.openInfoWindow({ 'content': 'Your authority is not recognized in fort kickass.' }, this);
+					});
 
-        var contentString = '<div id="content" style="width:200px;">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">Websense North America Office</h1>'+
-            '<div id="bodyContent">'+
-            '<ul><li>10240 Sorrento Valley Rd</li>'+
-			'<li>San Diego, California 92121</li>'+
-			'<li>Tel:<a href="tel:18007231166"> 1-800-723-1166</a></li>'+
-			'<li><a href="tel:18583208000">1-858-320-8000</a></li>'+
-			'<li>Fax: 1-858-458-2950</li>'+
-            '</div>'+
-            '</div>';
-
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            title: 'Websense North America Office'
-        });
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.open(map,marker);
-        });
-      }
-
-    </script>
+					// get current position
+					self.getCurrentPosition( function(position, status) {
+						if ( status === 'OK' ) {
+							var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+							//self.get('map').panTo(latlng);
+							self.search({ 'location': latlng }, function(results, status) {
+								if ( status === 'OK' ) {
+									$('#from').val(results[0].formatted_address);
+								}
+							});
+						} else {
+							alert('Unable to get current position');
+						}
+					});
+				
+					// submit
+					$('#submit').click(function() {
+						self.displayDirections({ 'origin': $('#from').val(), 'destination': $('#to').val(), 'travelMode': google.maps.DirectionsTravelMode.DRIVING }, { 'panel': document.getElementById('directions')}, function(response, status) {
+							if ( status !== 'OK' ) { 
+								$("#directions").html("We could not calculate directions between " + $("#from").val() + " and " + $("#from").val());
+							}
+							$('#results').show()
+						});
+						return false;
+					});
+				}});
+			});
+			
+        </script>
 
 	<style>
 		.ui-btn-inner {
@@ -67,26 +76,55 @@
 			margin:2px 0;
 		}
 		.ui-content {padding:0;}
-		html, body, #map_canvas {
+		/*html, body, #map_canvas {
         	margin: 0;
         	padding: 0;
         	height: 100%;
         	height:320px;
         	width:320px;
-      	}
+      	}*/
       	#firstHeading, #bodyContent ul {font-size:13px;}
 		
 	</style>
 </head>
-<body onload="initialize()">
+<body>
 <div data-role="page" class="ui-responsive-panel">
 	<?php include 'header.php'; ?>
 	<div data-role="content">
-		<div id="map_canvas"></div>
+		
+		<div id="directions_map">
+			<div data-role="content">	
+				<div class="ui-bar-c ui-corner-all ui-shadow" style="padding:1em;">
+					<div id="map_canvas_1" style="height:300px;"></div>
+					<p>
+						<label for="from">From</label>
+						<input id="from" class="ui-bar-c" type="text" placeholder="Your location" />
+					</p>
+					<p>
+						<label for="to">To</label>
+						<input id="to" class="ui-bar-c" type="text" value="10240 Sorrento Valley Rd San Diego, California 92121" />
+					</p>
+					<a id="submit" href="#" data-role="button" data-icon="search">Get directions</a>
+				</div>
+				<div id="results" class="ui-listview ui-listview-inset ui-corner-all ui-shadow" style="display:none;">
+					<div class="ui-li ui-li-divider ui-btn ui-bar-b ui-corner-top ui-btn-up-undefined">Results</div>
+					<div id="directions"></div>
+					<div class="ui-li ui-li-divider ui-btn ui-bar-b ui-corner-bottom ui-btn-up-undefined"></div>
+				</div>
+			</div>
+		</div>
+		
 	</div>
-	<div data-role="footer"></div>
+	<div data-role="footer">
+		
+		<div data-role="navbar">
+			<ul>
+				<li><a href="#">Contact Us</a></li>
+				<li><a href="#">Corporate Site</a></li>
+			</ul>
+		</div><!-- /navbar -->
+	</div>
 	 <?php include 'panel.php'; ?>
-	
 </div>
 </body>
 </html>
--- mobile/index3.php
+++ mobile/index3.php
@@ -30,9 +30,161 @@
 <div data-role="page" class="ui-responsive-panel">
 	<?php include 'header.php'; ?>
 	<div data-role="content">
-		<p>Some content</p>
+		<h2>Our most comprehensive information security solution.</h2>
+        <p>Websense&reg; TRITON™ Enterprise protects on-site and remote employees from the latest threats in a unified, hybrid solution. You
+        can add <a href="cybersecurity-intelligence-services-overview.aspx">Websense CyberSecurity Intelligence™ (CSI) services</a> to get online
+        malware sandboxing and direct access to <a href="http://securitylabs.websense.com/">Websense Security Labs™</a> threat researchers.</p>
+        <p>And as with all TRITON solutions, you get:</p>
+        <ul class="standard">
+            <li>Unified Architecture</li>
+            <li>Unified Security Intelligence</li>
+            <li>Unified Policy and Reporting</li>
+        </ul>
+        <p>Top Features</p>
+        <p><a href="web-security-gateway-anywhere-features.aspx">Web Security Gateway Anywhere</a></p>
+        <ul class="standard">
+            <li><b>Websense TruHybrid™ deployment</b> protects entire networks, including headquarters, branch offices and remote and mobile users. It enables provisioning through one unified interface.</li>
+            <li><b>Websense TruWeb DLP™ capability</b> prevents data loss and enables compliance. It includes unique features such as geo-location destination awareness and detection of criminal encrypted uploads, password file data theft, and slow data leaks (“drip” DLP). Other highlights include natural language processing, advanced machine learning and more than 1,600 policies and templates.</li>
+            <li><b>Gateway threat analysis</b> uses real-time, inline security analysis from <a href="websense-advanced-classification-engine.aspx">Websense ACE (Advanced Classification Engine)</a> and threat intelligence from the <a href="websense-threatseeker-network.aspx">Websense ThreatSeeker™ Network</a> to defend against advanced malware, blended threats and spam.</li>
+        </ul>
+        <p><a href="email-security-gateway-anywhere-features.aspx">Email Security Gateway Anywhere</a></p>
+        <ul class="standard">
+            <li><b>Websense TruHybrid™ deployment</b> conserves your network bandwidth by removing spam and threats from inbound email in the cloud. TruHybrid™ deployment is ISO 27001 certified and provides 99.999% service availability and 99% or higher spam detection.</li>
+            <li><b>Websense TruEmail DLP™ capability</b> offers enterprise-level protection against data theft and data loss for the SMTP channel. It includes natural language processing, data fingerprinting, mobile email DLP, incident forensics reporting, and more than 1,600 built-in templates for compliance to prevent confidential data from leaving the corporate network via email.</li>
+            <li><b>URL sandboxing</b> provides point-of-click threat analysis by isolating suspicious links embedded in emails and analyzing their destination when users click on them.</li>
+        </ul>
+        <p><a href="data-security-suite-features.aspx">Data Security Suite</a></p>
+        <ul class="standard">
+            <li><b>Websense PreciseID™ technology</b> identifies and classifies more than 370 different file types and formats — even when content is cut and pasted from one format to another. In conjunction with the <a href="websense-threatseeker-network.aspx">Websense ThreatSeeker Network</a>, PreciseID technology enables Data Security Suite to protect confidential data and prevent information leaks.</li>
+            <li><b>Over 1,600 pre-defined policies and over 1,100 pre-defined content classifiers</b> help identify and protect sensitive information. </li>
+            <li><b>Optical character recognition (OCR)</b> identifies sensitive data embedded within images and prevents loss. </li>
+        </ul>
+        <table class="lined">
+            <thead>
+                <tr>
+                    <th class="center">Feature </th>
+                    <th class="center span3">Description </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><b>TRITON Unified Security Center</b></td>
+                    <td class="span3">
+                    Reduces TCO by unifying management of web security, data security and email security components.
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>Malware protection</b></td>
+                    <td class="span3">
+                    <a href="websense-advanced-classification-engine.aspx">Websense ACE (Advanced Classification Engine)</a> scans inbound and outbound email for malware, spyware and targeted and blended threats. Combining real-time composite risk scoring with security intelligence from the <a href="websense-threatseeker-network.aspx">Websense ThreatSeeker&reg; Network</a>, ACE protects against both known and unknown threats within email.
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>Spam Detection </b></td>
+                    <td class="span3">
+                    Websense provides highly accurate spam blocking with very low false positives that is backed by a 99% or higher spam detection service level agreement. Spam is identified using a combination of technologies including sender reputation, adaptive learning, URL analysis, heuristics, suspicious PDF identification and optical recognition of image spam.          </td>
+                </tr>
+                <tr>
+                    <td><b>Enterprise Class DLP</b></td>
+                    <td class="span3">
+                    Expands DLP across all enterprise channels with a simple license key upgrade, letting you use a single management console to create and apply one policy to email, web and endpoints.          </td>
+                </tr>
+                <tr>
+                    <td><b>Mobile Email DLP</b></td>
+                    <td class="span3">
+                    Prevents users from accessing confidential emails and attachments on mobile devices.
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>Email Spooling for Disaster Recovery </b></td>
+                    <td class="span3">
+                    Built-in redundancy and email spooling ensures that email is never lost during a network or mail server outage.           </td>
+                </tr>
+                <tr>
+                    <td><b>Quarantine Management </b></td>
+                    <td class="span3">
+                    Includes a powerful search engine for complete visibility and access of quarantined messages and logs.
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>End-User Self Service </b></td>
+                    <td class="span3">
+                    Reduces administrative overhead by giving scheduled and on-demand access to users, enabling them to view and release quarantined messages and whitelist/blacklist senders.
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>Reporting</b></td>
+                    <td class="span3">
+                    Includes multiple report types to provide summary and detailed visibility into types and volumes of threats, number of messages processed, policy violations and more.
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>TLS Encryption </b></td>
+                    <td class="span3">
+                    Encrypts email between the sender's mail server and the receiver's mail server to prevent the email from being intercepted or altered while traversing the Internet.
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>Advanced Email Encryption* </b></td>
+                    <td class="span3">
+                    Encrypts the entire email and any attachments so that the contents are viewed only by intended recipient(s).
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>Email Archive* </b></td>
+                    <td class="span3">
+                    Every email that any person in your business sends and receives is securely archived, searchable and accessible forever in the cloud.
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>Image Analysis* </b></td>
+                    <td class="span3">
+                    Reliably detects pornographic images to ensure compliance with network use policy.
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+		<!--<video width="320" height="240" poster="http://www.websense.com/assets/imgs/2013/video-thumb-next-generation-email-security.png" preload="auto" controls>
+		  <source src="http://www.websense.com/assets/videos/EmailSecurity.mp4" type="video/mp4">
+		  <source src="assets/video/video-ace-en.mov" type="video/m4v">
+		  <source src="http://v2v.cc/~j/theora_testsuite/320x240.ogg">
+		  <source src="movie.ogg" type="video/ogg">
+		  Your browser does not support the video tag.
+		</video>-->
+		<div data-role="collapsible" data-theme="b" data-content-theme="c">
+				<h2>Download White Paper</h2>
+				<iframe width="280" height="450" src="http://a1.websense.com/defending-against-todays-targeted-phishing-attacks.html?sfcmpid=70120000000aR15" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
+		</div>
+		<ul data-role="listview" data-inset="true">
+			<li data-role="list-divider">Next Steps</li>
+			<li><a href="index.html">Tech Specs</a></li>
+			<li><a href="index.html">Product Resources</a></li>
+			<li><a href="index.html">Related Products</a></li>
+		</ul>
+		<script type="text/javascript">
+	  s.events='event1'; 
+	  s.eVar1='whitepapers'; 
+	</script>
+        <div class="mboxDefault"></div>
+        <script language="JavaScript1.2" type="text/javascript">mboxCreate('leadForm_conversion');</script>
+<!--		<div data-role="collapsible" data-theme="b" data-content-theme="c">
+			<h2>Next Steps</h2>
+			<ul data-role="listview">
+				<li><a href="index.html">Download White Paper</a></li>
+				<li><a href="index.html">Related Products</a></li>
+			</ul>
+		</div>-->
+<!--		<a href="index.html" data-role="button" data-inline="false" data-theme="b">Contact Us</a>-->
 	</div>
-	<div data-role="footer"></div>
+	<div data-role="footer">
+		
+		<div data-role="navbar">
+			<ul>
+				<li><a href="#">Contact Us</a></li>
+				<li><a href="#">Corporate Site</a></li>
+			</ul>
+		</div><!-- /navbar -->
+	</div>
 	 <?php include 'panel.php'; ?>
 	
 </div>
