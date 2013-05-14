d18751126f5cfe06bfc38da8b77602eb49496952
Craig Godfrey <repeater@gmail.com>
2013-05-14 12:10:02 -0700

    responsive complete
    saving myself!
    
    Signed-off-by: Craig Godfrey <repeater@gmail.com>

---
+++ mobile/m/content/cta.php
@@ -0,0 +1,16 @@
+<div class="wsCTA">
+	<div class="hpElement">
+		<a href="/content/page.php" alt="TRITON Solution Suites: Complete Security" class="hpElement">Websense 2013 Threat Report<br />
+			<span>Your trust in traditional security might be misplaced</span>
+		</a>
+	</div>
+	<div class="hpElement">
+		<a href="/content/page.php" alt="TRITON Solution Suites: Complete Security" class="hpElement">TRITON Solution Suites: Complete Security<br />
+			<span>Get web security, email security and DLP capabilities</span>
+		</a>
+	</div>
+	<div class="hpElement">
+		<a href="/content/page.php" alt="Our difference: Websense ACE" class="hpElement">Our difference: Websense ACE<br />
+			<span>Websense ACE is the power behined all our security products</span></a>
+	</div>
+</div>
--- mobile/m/content/home.php
+++ mobile/m/content/home.php
@@ -5,37 +5,24 @@
 <link rel="stylesheet" type="text/css" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" />
 <link rel="stylesheet" type="text/css" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile.structure-1.3.0.min.css" />
 <link rel="stylesheet" media="screen" type="text/css" href="/assets/css/mobile2013.css" />
+</head>
+<body class="wsHome">
+<div data-role="page" class="wsShadow">
+	<?php include 'header.php'; ?>
+	<?php include 'panel.php'; ?>
+	<div data-role="content">
+		<div class="wsHomeOne">
+			<p>It’s been a banner year for hackers, who managed to breach many of the world’s most trusted websites and cyber defenses. Mobile, social,email – all under siege. Find out why your go-to sites, social networks and even security providers are experiencing a crisis of trust.</p>
+			<?php include 'cta.php'; ?>
+		</div>
+	</div>
+	<?php include 'footer.php'; ?>
+</div>
 <script>
 	$(document).ready(function() {
 		$( "#navpanel" ).trigger( "updatelayout" );
 	});
 </script>
-</head>
-<body>
-<div data-role="page" class="wsHome wsShadow">
-	<?php include 'header.php'; ?>
-	<?php include 'panel.php'; ?>
-	<div data-role="content" class="wsHome">
-		<div class="wsHomeOne">
-			<p>It’s been a banner year for hackers, who managed to breach many of the world’s most trusted websites and cyber defenses. Mobile, social,email – all under siege. Find out why your go-to sites, social networks and even security providers are experiencing a crisis of trust.</p>
-			<div class="hpElement">
-				<a href="/content/page.php" alt="TRITON Solution Suites: Complete Security" class="hpElement">Websense 2013 Threat Report<br />
-					<span>Your trust in traditional security might be misplaced</span>
-				</a>
-			</div>
-			<div class="hpElement">
-				<a href="/content/page.php" alt="TRITON Solution Suites: Complete Security" class="hpElement">TRITON Solution Suites: Complete Security<br />
-					<span>Get web security, email security and DLP capabilities</span>
-				</a>
-			</div>
-			<div class="hpElement">
-				<a href="/content/page.php" alt="Our difference: Websense ACE" class="hpElement">Our difference: Websense ACE<br />
-					<span>Websense ACE is the power behined all our security products</span></a>
-			</div>
-		</div>
-	</div>
-	<?php include 'footer.php'; ?>
-</div>
 <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
 </body>
